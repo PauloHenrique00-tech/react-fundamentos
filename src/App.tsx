15bf285b144533be4d41df8422915d9e7f552ba4
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-23 11:41:00 -0300

    Exporta conteudo e rodape de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
+import Rodape from "./Rodape";
 
 export default function App() {
   // Comentário de uma linha
-  /* Comentário de múltiplas linhas*/
+  /* Comentário de múltiplas linhas */
   return (
     <>
       <Cabecalho />
@@ -11,49 +13,8 @@ export default function App() {
       {/* Ao usar outros elementos HTML no JSX, é importante 
       envolver tudo em único <> </> (Fragmento React) */}
 
-      <main>
-        <section>
-          <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
-          <p>Este é um exemplo de aplicação React.</p>
-
-          <article>
-            <h3> Artigo 1 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
-              praesentium harum assumenda atque exercitationem facilis commodi
-              laborum quas, dicta, eveniet iste accusamus vero impedit,
-              architecto rem. Fuga, culpa. Reiciendis.
-            </p>
-          </article>
-
-          <article>
-            <h3> Artigo 2 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
-              praesentium harum assumenda atque exercitationem facilis commodi
-              laborum quas, dicta, eveniet iste accusamus vero impedit,
-              architecto rem. Fuga, culpa. Reiciendis.
-            </p>
-          </article>
-
-          <article>
-            <h3> Artigo 3 de exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
-              praesentium harum assumenda atque exercitationem facilis commodi
-              laborum quas, dicta, eveniet iste accusamus vero impedit,
-              architecto rem. Fuga, culpa. Reiciendis.
-            </p>
-          </article>
-        </section>
-      </main>
-
-      <footer>
-        <h2>Pequeno site criado com React</h2>
-        <p>
-          Desenvolvido por <b>Paulo</b> &copy; 2025
-        </p>
-      </footer>
+      <Conteudo />
+      <Rodape />
     </>
   );
 }
---
+++ src/Conteudo.tsx
@@ -0,0 +1,40 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+        <p>Este é um exemplo de aplicação React.</p>
+
+        <article>
+          <h3> Artigo 1 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
+            praesentium harum assumenda atque exercitationem facilis commodi
+            laborum quas, dicta, eveniet iste accusamus vero impedit, architecto
+            rem. Fuga, culpa. Reiciendis.
+          </p>
+        </article>
+
+        <article>
+          <h3> Artigo 2 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
+            praesentium harum assumenda atque exercitationem facilis commodi
+            laborum quas, dicta, eveniet iste accusamus vero impedit, architecto
+            rem. Fuga, culpa. Reiciendis.
+          </p>
+        </article>
+
+        <article>
+          <h3> Artigo 3 de exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
+            praesentium harum assumenda atque exercitationem facilis commodi
+            laborum quas, dicta, eveniet iste accusamus vero impedit, architecto
+            rem. Fuga, culpa. Reiciendis.
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+}
