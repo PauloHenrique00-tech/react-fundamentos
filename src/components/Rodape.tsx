33135c34ba9a9bc9a8f32074b3165639231bcbb4
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-24 10:03:59 -0300

    Cria e importa CSS inline

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,7 +1,16 @@
+import type { CSSProperties } from "react";
+
+const estilosrodape: CSSProperties = {
+  backgroundColor: "#222",
+  color: "white",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
     <>
-      <footer>
+      <footer style={estilosrodape}>
         <h2>Pequeno site criado com React</h2>
         <p style={{ color: "red", fontWeight: "bold" }}>
           Desenvolvido por <b>Paulo</b> &copy; 2025
