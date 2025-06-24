4548badc8cea6bf913927ccc113f0ad14d48da05
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-24 09:14:31 -0300

    Cria e reutiliza componente Artigo

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Artigo() {
+  return (
+    <article>
+      <h3> Artigo 1 de exemplo</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
+        praesentium harum assumenda atque exercitationem facilis commodi laborum
+        quas, dicta, eveniet iste accusamus vero impedit, architecto rem. Fuga,
+        culpa. Reiciendis.
+      </p>
+    </article>
+  );
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,35 +7,11 @@ export default function Conteudo() {
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
-        <article>
-          <h3> Artigo 1 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
-            praesentium harum assumenda atque exercitationem facilis commodi
-            laborum quas, dicta, eveniet iste accusamus vero impedit, architecto
-            rem. Fuga, culpa. Reiciendis.
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3> Artigo 2 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
-            praesentium harum assumenda atque exercitationem facilis commodi
-            laborum quas, dicta, eveniet iste accusamus vero impedit, architecto
-            rem. Fuga, culpa. Reiciendis.
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3> Artigo 3 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non natus
-            praesentium harum assumenda atque exercitationem facilis commodi
-            laborum quas, dicta, eveniet iste accusamus vero impedit, architecto
-            rem. Fuga, culpa. Reiciendis.
-          </p>
-        </article>
+        <Artigo />
       </section>
     </main>
   );
