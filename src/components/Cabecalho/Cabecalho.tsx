815b2ac3eefae54846e0f5650d045f3cdd42ff30
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-25 08:57:29 -0300

    Reorganiza componentes e módulos

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,8 @@
 /* Tudo que for usado dentro de componentes (imagens, fontes etc),
 deve ser importado. Desta forma, no momento do build, haverá otimizações para
 este conteúdo. */
-import logo from "../assets/logo-react.png";
-import Menu from "./Menu";
+import logo from "../../assets/logo-react.png";
+import Menu from "../Menu/Menu";
 
 /* Importando o módulo CSS para o componente Cabecalho */
 import estilos from "./Cabecalho.module.css";
