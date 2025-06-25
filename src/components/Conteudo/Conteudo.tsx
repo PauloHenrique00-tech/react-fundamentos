6716aa109e080cfed4249192e9bec48fa9581593
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-25 11:48:56 -0300

    Aplica classes tailwind

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -1,6 +1,6 @@
-import Artigo from "../Artigo/Artigo";
-import AvisoImporante from "../AvisoImportante/AvisoImporante";
-import DicaDoDia from "../DicaDoDia/DicaDoDia";
+import Artigo from "./Artigo/Artigo";
+import AvisoImporante from "./AvisoImportante/AvisoImporante";
+import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import estilos from "./Conteudo.module.css";
 
 export default function Conteudo() {
@@ -9,7 +9,9 @@ export default function Conteudo() {
       <section
         className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
       >
-        <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+        <h2 className="font-bold text-lg">
+          Seja bem-vindo(a) aos Fundamentos de React!
+        </h2>
         <p>Este é um exemplo de aplicação React.</p>
 
         <Artigo />
