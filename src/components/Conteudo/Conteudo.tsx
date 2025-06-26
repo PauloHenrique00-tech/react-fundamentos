3b64a04f52f4711daebe5cf1ce569c6952cacbd7
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-26 11:27:17 -0300

    Aplica map para carregar Artigo com dados do curso

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -3,6 +3,7 @@ import AvisoImportante from "./AvisoImportante/AvisoImportante";
 import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import estilos from "./Conteudo.module.css";
 import Saudacao from "./Saudacao";
+import cursos from "../../data/cursos";
 
 export default function Conteudo() {
   return (
@@ -12,34 +13,15 @@ export default function Conteudo() {
       >
         <h2 className="font-bold text-lg">Fundamentos de React!</h2>
         <Saudacao nome="Paulo" classe="bg-emerald-100" />
-        <Saudacao nome="Ricardo" classe="bg-red-100" />
 
         <p>Este é um exemplo de aplicação React.</p>
 
-        <Artigo titulo="Front-End" numero={1} data={new Date()}>
-          <p>Front-End é muito bacana, a gente usa HTML com CSS e JavaScript</p>
-          <p>Mas também dá dor de cabeça quando usa CSS...</p>
-        </Artigo>
+        {/* Faça a lógica necessária para apresentar o componente Artigo e passar para ele 
+        os dados de cada curso proveniente de cursos. */}
 
-        <Artigo titulo="Back-End" numero={2}>
-          <p>Importante estudar:</p>
-          <ul className="*:list-disc *:ml-7">
-            <li>PHP</li>
-            <li>SQL</li>
-            <li>Orientação a Objetos</li>
-          </ul>
-          <p>E muito mais!</p>
-        </Artigo>
-
-        <Artigo titulo="Mobile" numero={3}>
-          <details>
-            <summary>Conheça as tecnologias mais usadas</summary>
-            <ol className="*:list-decimal *:ml-7">
-              <li>JavaScript/TypeScript</li>
-              <li>Libs e Frameworks</li>
-            </ol>
-          </details>
-        </Artigo>
+        {cursos.map((curso) => {
+          return <Artigo dados={curso} key={curso.id} />;
+        })}
       </section>
 
       <DicaDoDia />
