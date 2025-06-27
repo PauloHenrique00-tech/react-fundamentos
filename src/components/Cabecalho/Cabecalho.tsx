7b353cab644b9663517bf722f919f474d2121b2d
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-27 09:36:28 -0300

    Adiciona state para título

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,17 +1,22 @@
-/* Tudo que for usado dentro de componentes (imagens, fontes etc),
-deve ser importado. Desta forma, no momento do build, haverá otimizações para
-este conteúdo. */
 import logo from "../../assets/logo-react.png";
 import Menu from "./Menu/Menu";
-
-/* Importando o módulo CSS para o componente Cabecalho */
 import estilos from "./Cabecalho.module.css";
+import { useState } from "react";
 
 export default function Cabecalho() {
+  /* Aqui definimos a chamada de uma função especial 
+  do React: use State. Esta e outras funções iniciadas com
+  o prefixo 'use' são conhecidas como React Hooks.
+
+  No caso do useState, usando desestruturação, criamos
+  uma constante que representa o valor do state (titulo)
+  e uma função responsável por atualizar o state (setTitulo) */
+  const [titulo, setTitulo] = useState("Olá React!");
+
   return (
     <header className={estilos.topoSite}>
       <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
-        <img className={estilos.logo} src={logo} alt="" /> Olá React! 😊
+        <img className={estilos.logo} src={logo} alt="" /> {titulo} 😊
       </h1>
       <hr />
       <Menu />
