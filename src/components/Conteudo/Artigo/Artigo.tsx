5e3a60cb387ae22b221275d26da580dac6deda88
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-27 11:00:28 -0300

    Aplica State de cor

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,5 +1,6 @@
 import estilos from "./Artigo.module.css";
 import type { Curso } from "../../../types/Curso";
+import { useState } from "react";
 
 type ArtigoProps = {
   dados: Curso;
@@ -7,9 +8,20 @@ type ArtigoProps = {
 
 export default function Artigo({ dados }: ArtigoProps) {
   // Podemos receber a prop dados para depois desestruturar:
+
+  const [artigo, setArtigo] = useState(false);
+
+  const alternarCor = () => {
+    setArtigo((artigo) => !artigo);
+  };
+
   const { titulo, preco, categoria } = dados;
   return (
-    <article className={estilos.artigo}>
+    <article
+      className={estilos.artigo}
+      style={{ backgroundColor: artigo ? "yellow" : "white" }}
+      onClick={alternarCor}
+    >
       <h3>{titulo}</h3>
       <p>
         <b>Categoria:</b>
