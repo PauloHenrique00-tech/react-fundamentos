34d57e517f881959820b28627b31af3538b264d7
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-06-27 08:49:21 -0300

    Update ListaCursos.tsx

--- src/components/ListaCursos/ListaCursos.tsx
+++ src/components/ListaCursos/ListaCursos.tsx
@@ -2,8 +2,6 @@ import estilos from "./ListaCursos.module.css";
 import Artigo from "../Conteudo/Artigo/Artigo";
 import cursos from "../../data/cursos";
 
-import React from "react";
-
 export default function ListaCursos() {
   return (
     <div className={estilos.artigos}>
