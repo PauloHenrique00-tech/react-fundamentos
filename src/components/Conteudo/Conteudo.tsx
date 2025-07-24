fdfc5dced752d1fed6a42414485c223e3f8de76e
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-07-24 11:52:03 -0300

    Ajusta CSS

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -1,6 +1,5 @@
 import AvisoImportante from "./AvisoImportante/AvisoImportante";
 import DicaDoDia from "./DicaDoDia/DicaDoDia";
-import estilos from "./Conteudo.module.css";
 import Saudacao from "./Saudacao";
 import cursos from "../../data/cursos";
 import ListaCursos from "../ListaCursos/ListaCursos";
@@ -16,9 +15,7 @@ export default function Conteudo() {
 
   return (
     <>
-      <section
-        className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
-      >
+      <section>
         <h2 className="font-bold text-lg">Fundamentos de React!</h2>
         <Saudacao nome="Paulo" classe="bg-emerald-100" />
 
