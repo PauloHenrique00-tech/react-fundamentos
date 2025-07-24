83c2a6ef0004fb88ba4b9b193b49078a75b83a03
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-07-24 09:55:56 -0300

    Cria componentes para as páginas

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,13 @@
+export default function Sobre() {
+  return (
+    <section>
+      <h2>Sobre nossa empresa</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam,
+        perspiciatis. Odit repudiandae blanditiis magni perspiciatis nesciunt
+        veniam dignissimos fugit! Eos tempora quas temporibus nobis earum? Sint
+        voluptatibus quaerat odio quis?
+      </p>
+    </section>
+  );
+}
